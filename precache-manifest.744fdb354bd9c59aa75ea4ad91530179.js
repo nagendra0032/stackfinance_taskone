563baa099c00c531d3850b64b9fb223ca4c1c9ac
nagendra0032 <nagendrakumar0032@gmail.com>
2020-02-07 01:15:59 +0530

    Updates

---
+++ precache-manifest.744fdb354bd9c59aa75ea4ad91530179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0482589e9ee4e7107433fc091c8098a2",
+    "url": "/stackfinance_taskone/index.html"
+  },
+  {
+    "revision": "985f371652b63dea031c",
+    "url": "/stackfinance_taskone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3990d6d5ad04723a7408",
+    "url": "/stackfinance_taskone/static/js/2.8efc4081.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/stackfinance_taskone/static/js/2.8efc4081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985f371652b63dea031c",
+    "url": "/stackfinance_taskone/static/js/main.ec83d9ca.chunk.js"
+  },
+  {
+    "revision": "ab07a05caaf06d992316",
+    "url": "/stackfinance_taskone/static/js/runtime-main.994723d0.js"
+  }
+]);
